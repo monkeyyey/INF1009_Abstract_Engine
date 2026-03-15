c6e19678155ca7aa9f725ff83c9942264ac26da5
monkeyyey <zehchern@gmail.com>
2026-03-15 12:34:59 +0800

    animation

--- core/src/main/java/com/myGame/simulation/animation/AnimationComponent.java
+++ core/src/main/java/com/myGame/simulation/animation/AnimationComponent.java
@@ -1,4 +1,4 @@
-package com.myGame.simulation.animation;
+    package com.myGame.simulation.animation;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
